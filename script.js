de2d9cd9559e390552cbb1ac04c2ff2f9f20aed6
Kordian-Napoleon <kordian.napoleon@gmail.com>
2024-11-30 18:58:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -206,6 +206,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 gifConteiner.classList.add('active');
             };
 
+            if(blackCounter == 5){alert("AAAAAAAA!!!")};
             createGif();
         }
     };
